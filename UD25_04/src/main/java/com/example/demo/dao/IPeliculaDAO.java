1e037211fcd10e9a5dc7d8a9363161952cc808ca
Nayara <nay.nivea@gmail.com>
2023-12-05 13:36:07 +0100

    Correción

--- UD25_04/src/main/java/com/example/demo/dao/IPeliculaDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.dto.Pelicula;
-
-public interface IPeliculaDAO extends JpaRepository<Pelicula, Integer> {
-
-}
